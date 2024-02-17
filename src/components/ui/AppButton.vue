c17a04ea2d765bcaf9efb0bb2a95bfa018d80a53
gartonot <gartonot62@gmail.com>
2024-02-17 18:18:44 +0300

    Create component AppButton

---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+type TButton = 'a' | 'button'
+
+interface IProps {
+  label: string,
+  outlined?: boolean,
+  href?: string,
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  outlined: false,
+  href: '',
+})
+
+const buttonComponent: TButton = props.href.length ? 'a' : 'button'
+</script>
+
+<template>
+  <component 
+    :is="buttonComponent"
+    :href="href"
+    class="button"
+  >
+    {{ label }}
+  </component>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  cursor: pointer;
+  background-color: transparent;
+  padding: 11px 20px;
+  color: $color-white;
+  background-color: $color-primary;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  font-size: 16px;
+  line-height: 16px;
+}
+</style>
