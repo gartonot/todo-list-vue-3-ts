cd56a00332c50438bfd34c65a6889489293188eb
gartonot <gartonot62@gmail.com>
2024-02-17 18:20:21 +0300

    Added component TheLogo

---
+++ src/components/TheLogo.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+interface IProps {
+  href?: string
+}
+
+withDefaults(defineProps<IProps>(), {
+  href: '/',
+});
+
+</script>
+
+<template>
+  <a :href="href" class="logo">
+    ToDo
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  font-weight: 800;
+  font-size: 20px;
+  line-height: 27px;
+}
+</style>
