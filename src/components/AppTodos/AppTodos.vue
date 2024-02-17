13d65aa85480e02902cd3e8f35054321fc2d4123
gartonot <gartonot62@gmail.com>
2024-02-17 23:44:57 +0300

    Added todoList and design for todoItem

--- src/components/AppTodos/AppTodos.vue
+++ src/components/AppTodos/AppTodos.vue
@@ -3,20 +3,40 @@ import TodoItem from '@/components/AppTodos/TodoItem.vue';
 import { ITodoItem } from '@/interfaces';
 import { EnumCategory, EnumStatusTodo } from '@/enums';
 
-const todoList: ITodoItem[] = [
-{ 
+let todoList: ITodoItem[] = [
+  { 
     id: 0,
     title: 'Создание логотипа',
-    category: EnumCategory.DESIGN,
-    status: EnumStatusTodo.IN_PROGRESS,
+    category: {
+      name: EnumCategory.DESIGN,
+      color: EnumCategory.DESIGN_COLOR,
+    },
+    status: EnumStatusTodo.TODO,
+    text: 'Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. Один из проверенных способов это сделать — помочь благотворительному фонду «Луч Добра». Благодаря их труду ежегодно сотни питомцев находят свой новый дом.',
   },
   { 
     id: 1,
     title: 'Создание логотипа',
-    category: EnumCategory.TESTING,
+    category: {
+      name: EnumCategory.TESTING,
+      color: EnumCategory.TESTING_COLOR,
+    },
     status: EnumStatusTodo.IN_PROGRESS,
+    text: 'Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. Один из проверенных способов это сделать — помочь благотворительному фонду «Луч Добра». Благодаря их труду ежегодно сотни питомцев находят свой новый дом.',
+  },
+  { 
+    id: 2,
+    title: 'Создание логотипа',
+    category: {
+      name: EnumCategory.DEVELOP,
+      color: EnumCategory.DEVELOP_COLOR,
+    },
+    status: EnumStatusTodo.COMPLETED,
+    text: 'Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. Один из проверенных способов это сделать — помочь благотворительному фонду «Луч Добра». Благодаря их труду ежегодно сотни питомцев находят свой новый дом.',
   },
 ]
+
+todoList = [...todoList, ...todoList, ...todoList, ...todoList, ...todoList]; 
 </script>
 
 <template>
@@ -30,10 +50,30 @@ const todoList: ITodoItem[] = [
         v-for="todo in todoList"
         :key="todo.id"
         :todo="todo"
+        class="todo-list__item"
       />
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+.todo-list {
+  --gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--gap);
+  margin-top: 24px;
+
+  &__item {
+    --count-card: 1;
+    flex: 1 0 calc(100% / var(--count-card) - var(--gap));
+
+    @include media-mobile {
+      --count-card: 2;
+    }
+    @include media-tablet {
+      --count-card: 3;
+    }
+  }
+}
 </style>
--- src/components/AppTodos/TodoItem.vue
+++ src/components/AppTodos/TodoItem.vue
@@ -1,15 +1,114 @@
 <script lang="ts" setup>
+import MoreHorizIcon from '@/components/icons/MoreHorizIcon.vue'
+
 import type { ITodoItem } from '@/interfaces'
 
 interface IProps {
   todo: ITodoItem
 }
 
-defineProps<IProps>()
+const props = defineProps<IProps>()
+
+const categoryColor = props.todo.category.color;
 </script>
 
 <template>
-  <div class="todo-item">
-    {{ todo }}
-  </div>
+  <article class="todo">
+    <header class="todo__header">
+      <div class="category">
+        {{ todo.category.name }}
+      </div>
+      <button class="actions">
+        <MoreHorizIcon />
+      </button>
+    </header>
+    <p class="todo__title">
+      {{ todo.title }}
+    </p>
+    <div class="todo__body">
+      {{ todo.text }}
+    </div>
+  </article>
 </template>
+
+<style lang="scss" scoped>
+.todo {
+  border: $border-default;
+  border-radius: 8px;
+  padding: 16px;
+  padding-bottom: 0;
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+
+    .category {
+      --font-size: 12px;
+      --line-height: 12px;
+      font-size: var(--font-size);
+      line-height: var(--line-height);
+      color: v-bind(categoryColor);
+      font-weight: 500;
+
+      @include media-mobile {
+        --font-size: 14px;
+        --line-height: 14px;
+      }
+      @include media-tablet {
+        --font-size: 16px;
+        --line-height: 16px;
+      }
+    }
+    .actions {
+      --size: 24px;
+      width: var(--size);
+      height: var(--size);
+      border-radius: 50%;
+      background-color: transparent;
+      border: $border-default;
+      color: $color-black;
+    }
+  }
+  &__title {
+    --line-height: 22px;
+    --font-size: 16px;
+    font-weight: 600;
+    font-size: var(--font-size);
+    width: 100%;
+
+    @include multiline-clip(var(--line-height));
+
+    @include media-mobile {
+      --line-height: 24px;
+      --font-size: 18px;
+    }
+  }
+  &__body {
+    --line-height: 16px;
+    --font-size: 14px;
+    margin-top: 8px;
+    position: relative;
+
+    font-size: var(--font-size);
+    line-height: var(--line-height);
+
+
+    @include multiline-clip(var(--line-height), 3);
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(transparent, #FFFFFF 65%);
+    }
+
+    @include media-mobile {
+      --line-height: 20px;
+      --font-size: 16px;
+    }
+  }
+}
+</style>
