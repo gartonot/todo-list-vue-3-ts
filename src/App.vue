a0bc3177a085681a606df2b64eea4abe49c3ee1f
gartonot <gartonot62@gmail.com>
2024-02-17 19:12:40 +0300

    Added AppTodos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script lang="ts" setup>
 import TheHeader from '@/components/TheHeader.vue';
-
+import AppTodos from '@/components/AppTodos/AppTodos.vue';
 </script>
 
 <template>
   <div class="app">
     <TheHeader />
+    <AppTodos class="app__todos" />
   </div>
 </template>
 
 <style lang="scss" scoped>
 .app {
   padding-top: 26px;
+
+  &__todos {
+    margin-top: 35px;
+  }
 }
 </style>
---
+++ src/components/AppTodos/AppTodos.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <section class="container">
+    <h1>Список задач</h1>
+    <!-- Filter --> 
+    <!-- TaskList -->
+  </section>
+</template>
+
+<style lang="scss" scoped>
+</style>
