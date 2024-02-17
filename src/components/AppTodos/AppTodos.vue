6ed9414335d675eaeadb1a8035551edb1b530f41
gartonot <gartonot62@gmail.com>
2024-02-17 22:44:25 +0300

    Added TodoItem

--- src/components/AppTodos/AppTodos.vue
+++ src/components/AppTodos/AppTodos.vue
@@ -1,4 +1,22 @@
 <script lang="ts" setup>
+import TodoItem from '@/components/AppTodos/TodoItem.vue';
+import { ITodoItem } from '@/interfaces';
+import { EnumCategory, EnumStatusTodo } from '@/enums';
+
+const todoList: ITodoItem[] = [
+{ 
+    id: 0,
+    title: 'Создание логотипа',
+    category: EnumCategory.DESIGN,
+    status: EnumStatusTodo.IN_PROGRESS,
+  },
+  { 
+    id: 1,
+    title: 'Создание логотипа',
+    category: EnumCategory.TESTING,
+    status: EnumStatusTodo.IN_PROGRESS,
+  },
+]
 </script>
 
 <template>
@@ -6,6 +24,14 @@
     <h1>Список задач</h1>
     <!-- Filter --> 
     <!-- TaskList -->
+    
+    <div class="todo-list">
+      <TodoItem
+        v-for="todo in todoList"
+        :key="todo.id"
+        :todo="todo"
+      />
+    </div>
   </section>
 </template>
 
---
+++ src/components/AppTodos/TodoItem.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { ITodoItem } from '@/interfaces'
+
+interface IProps {
+  todo: ITodoItem
+}
+
+defineProps<IProps>()
+</script>
+
+<template>
+  <div class="todo-item">
+    {{ todo }}
+  </div>
+</template>
